6ee7ba0938226c5a864594f32fcdb8b72e9c9d1c
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-07-20 18:01:49 -0700

    Track Discord chat activity for rain eligibility

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { ActivityType, Client, Events, GatewayIntentBits } from 'discord.js';
+import { ActivityTracker } from './activity-tracker.js';
 import { buildCommands } from './commands.js';
 import { loadConfig } from './config.js';
 import { buildReactionDropCommand } from './reaction-drop.js';
@@ -15,11 +16,16 @@ console.log(`Connected to MySQL database ${dbInfo.db}`);
 
 const rpc = new YerbasRpc(config.rpc);
 const walletWorker = new WalletWorker({ config, ledger, rpc });
-const context = { config, ledger, rpc };
+const activityTracker = new ActivityTracker();
+const context = { config, ledger, rpc, activityTracker };
 const commands = [...buildCommands(context), buildReactionDropCommand(context)];
 const commandMap = new Map(commands.map((command) => [command.data.name, command]));
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessageReactions]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions
+  ]
 });
 
 client.once(Events.ClientReady, (readyClient) => {
@@ -28,6 +34,10 @@ client.once(Events.ClientReady, (readyClient) => {
   console.log(`Yerbas Tip Bot v2 connected as ${readyClient.user.tag}`);
   console.log(`Wallet: ${config.walletEnabled ? 'enabled' : 'disabled'}; withdrawals: ${config.withdrawalsEnabled ? 'enabled' : 'disabled'}`);
   console.log(`Assets: ${config.assetsEnabled ? 'enabled' : 'disabled'}; asset withdrawals: ${config.assetWithdrawalsEnabled ? 'enabled' : 'disabled'}`);
+});
+
+client.on(Events.MessageCreate, (message) => {
+  activityTracker.record(message);
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
