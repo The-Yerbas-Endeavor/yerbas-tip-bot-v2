d32052931bcb0ce1e3e649d61f097b3c03e1806e
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-07-20 17:39:08 -0700

    Enable reaction drops at runtime

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { ActivityType, Client, Events, GatewayIntentBits } from 'discord.js';
 import { buildCommands } from './commands.js';
 import { loadConfig } from './config.js';
+import { buildReactionDropCommand } from './reaction-drop.js';
 import './services/mysql-ledger-admin.js';
 import { MySqlLedger } from './services/mysql-ledger.js';
 import { YerbasRpc } from './services/yerbas-rpc.js';
@@ -14,9 +15,12 @@ console.log(`Connected to MySQL database ${dbInfo.db}`);
 
 const rpc = new YerbasRpc(config.rpc);
 const walletWorker = new WalletWorker({ config, ledger, rpc });
-const commands = buildCommands({ config, ledger, rpc });
+const context = { config, ledger, rpc };
+const commands = [...buildCommands(context), buildReactionDropCommand(context)];
 const commandMap = new Map(commands.map((command) => [command.data.name, command]));
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessageReactions]
+});
 
 client.once(Events.ClientReady, (readyClient) => {
   readyClient.user.setPresence({ activities: [{ name: config.botStatus, type: ActivityType.Watching }], status: 'online' });
